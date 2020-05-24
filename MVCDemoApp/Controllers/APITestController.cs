5277ed0b3cd2b62278ecec30e2520f23fb85b2cd
Indy <hirst.geoffrey@gmail.com>
2020-05-24 20:39:59 +0100

    API adding, to link

---
+++ MVCDemoApp/Controllers/APITestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCDemoApp.Models;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APITestController : ControllerBase
+    { 
+        private readonly ILogger<APITestController> _logger;
+
+        public APITestController(ILogger<APITestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Employee
+            {
+                //Date = DateTime.Now.AddDays(index),
+                //TemperatureC = rng.Next(-20, 55),
+                //Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MVCDemoApp/Models/APITestPage.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MVCDemoApp.Controllers
+{
+    public class APITestController
+    {
+        public DateTime Date { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+    }
+}
