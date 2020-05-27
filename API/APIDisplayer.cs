708717f1dc63e5a2ffdfd5e9a5bc76b929c49f96
Indy <hirst.geoffrey@gmail.com>
2020-05-27 18:04:27 +0100

    unduplicating

--- API/APIDisplayer.cs
+++ API/APIDisplayer.cs
@@ -5,7 +5,6 @@ namespace MVCDemoApp
     public class APIDisplayer
     {
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
